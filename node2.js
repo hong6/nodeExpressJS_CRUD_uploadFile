67a5cc39d4678ce13f1948389ed63519606d63c8
xiaohong6 <xiaohongliu2006@gmail.com>
2014-01-06 22:38:16 -0500

    first commit v1.0.0

---
+++ node2.js
@@ -0,0 +1,7 @@
+﻿var http = require('http');    
+
+var server = http.createServer(function onRequest(req, res) {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.write('here is content.');
+    res.end();
+}).listen(3000);
